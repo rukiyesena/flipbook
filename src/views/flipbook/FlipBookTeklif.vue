8c0ff7405844cd6a43937b50cb4ffc78c1ce22a2
evrimt99 <terkevrim@gmail.com>
2024-01-22 13:26:16 +0300

    teklif sayfası

--- src/views/flipbook/FlipBookTeklif.vue
+++ src/views/flipbook/FlipBookTeklif.vue
@@ -10,14 +10,36 @@
 
           <div class="sender-address-container">
             <div class="sender-details">
-              <h6>Sender: Your Name</h6>
-              <h6>Address: Your Address</h6>
+              <h3>FATİH PROFİL SANAYİ VE TİCARET A.Ş.</h3>
+              <h6><strong>ADRES::</strong> Organize Sanayi 1. Kısım Turan Bahadır Cad. No:15 Honaz/DENİZLİ</h6>
+              <h6><strong>TELEFON:</strong> +90 258 269 16 64 </h6>
+              <h6><strong>FAKS:</strong> +90 258 269 11 41  </h6>
+              <h6><strong>E-POSTA:</strong> pazarlama@fatih.com.tr </h6>
+              <h6><strong>VERGİ D.:</strong> Saraylar Vergi Dairesi  </h6>
+              <h6><strong>VERGİ NO:</strong> 385 001 70 69 </h6>
             </div>
+            
+            
 
             <!-- Sağ taraftaki alan -->
-            <div class="right-panel" style="margin-left: 1254px ;">
-              <h6>Sender: Your Name</h6>
-              <h6>Address: Your Address</h6>            </div>
+            <div class="right-panel" style="margin-left: 854px; margin-top: -134px;">
+              <h6>Yetkili: <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+
+              <h6>Adres: <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+                <h6> TELEFON: 
+                <input type="number" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+              
+                <h6>FAKS: <input type="number" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+
+                <h6>E-POSTA:
+                  <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+
+                <h6>VERGİ D.:
+                  <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+              
+                <h6>VERGİ NO: 
+                <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 300px;" /></h6>
+              </div>
           </div>
 
 
@@ -59,7 +81,9 @@
               <vs-th style="color: black; font-weight: bold;">
                 Tutar
               </vs-th>
-
+              <vs-th style="color: black; font-weight: bold;">
+                Termin
+              </vs-th>
             </template>
       
             <template slot-scope="{ data }">
@@ -68,7 +92,7 @@
                   {{ data[indextr].stockCode }}
                 </vs-td>
                 <vs-td style="color: black; font-weight: bold;">
-                  <img :src="data[indextr].url_image" alt="Product Image" style="max-width: 350px; max-height: 200px;">
+                  <img :src="data[indextr].url_image" alt="Product Image" style="max-width: 150px; max-height: 150px;">
                 </vs-td>
               
                 <vs-td :data="data[indextr].attr3_boy" style="color: black; font-weight: bold;">
@@ -96,17 +120,18 @@
                 
                 <vs-td>
                   <div>
-                    <!-- Bind the computed property 'tutarForItem' to the input field -->
                     <input type="text" class="styled-input" style="color: black; font-weight: bold; font-size: 12px; height: 30px; width: 40px;" :value="tutarForItem(indextr)" disabled />
                   </div>
                 </vs-td>
               </vs-tr>
-              <b-col>
-                {{ toplamTutar }}
-              </b-col>
+      
               <b-col>
             
-            
+                <vs-td>
+                  <div>
+                    <input type="date" class="styled-input" style="color: black; font-weight: bold; font-size: 12px; height: 30px; width: 40px;" :value="termin(indextr)"  />
+                  </div>
+                </vs-td>
               </b-col>
               <b-col>
       
@@ -114,9 +139,8 @@
             </b-col>
       
             </template>          </vs-table>
-
-            <div class="additional-fields" style="margin-left: 1111px;">
-      
+            <div style="display: table; width: 100%;">
+              <div class="additional-fields" style=" margin-left: 1112px;" >      
             
               <div>
                 <vs-th style="color: black; font-weight: bold;">TOPLAM</vs-th>
@@ -136,7 +160,15 @@
                 </vs-td>
               </div>
               <br>
-
+              <div>
+                <vs-th style="color: black; font-weight: bold;">  GENEL İSKONTO </vs-th>
+                <vs-td>
+                  <div>
+                    <input type="text" class="styled-input" style="color: rgb(2, 2, 2); font-weight: bold; height: 30px; width: 120px;" :value="genelİskonto"  />
+                  </div>
+                </vs-td>
+              </div>
+              <br>
               <div>
                 <vs-th style="color: black; font-weight: bold;">GENEL TOPLAM</vs-th>
                 <vs-td>
@@ -145,11 +177,70 @@
                   </div>
                 </vs-td>
               </div>
+             
             </div>
             
+          </div>
+<b-col> </b-col>
+<div class="not-panel" style=" margin-top: -140px;">
+  <h6> Not1: Siparişin onaylanması için bu form imzalanarak tarafımıza ulaştırılmalı ve ilgili ödeme gerçekleşmiş olmalıdır.</h6>
+  <h6> Not2: Geçerlilik Süresi sonrasında onaylanan teklifler için fiyat, stok ve sevk tarihi bilgileri değişebilir.</h6>
+  <h6> Not3: Üretim hataları dışında iade kabul edilmemektedir. | Firmamız e-Fatura mükellefidir.</h6>
+  <h6>Not4: <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 500px;" /></h6>
+  <h6>Not5: <input type="text" class="styled-input" style="color: black; font-weight: bold; width: 500px;" /></h6>
+  </div>
+
+
+
+ 
             
-            <br>
-            <br>
+
+
+<br>
+<br>
+
+<br>
+
+<br>
+
+<div class="birinciAlan"  style="display: inline-flex;">
+            
+              <div>
+                <vs-th style="color: black; font-weight: bold;">Geçerlilik Süresi</vs-th>
+                <vs-td>
+                  <div>
+                    <input type="text" class="styled-input" style="color: black; font-weight: bold; height: 30px; width: 160px;" v-model="gecerlilikSuresi" disabled />
+                  </div>
+                </vs-td>
+              </div>
+              <br>
+              <div>
+                <vs-th style="color: black; font-weight: bold; ">Sevk Tarihi </vs-th>
+                <vs-td>
+                  <div>
+                    <input type="text" class="styled-input" style="color: black; font-weight: bold; height: 30px; width: 160px;" :value="sevkTarihi" />
+                  </div>
+                </vs-td>
+              </div>
+              <br>
+              <div>
+                <vs-th style="color: black; font-weight: bold;">  Nakliye </vs-th>
+                <vs-td>
+                  <div>
+                    <input type="text" class="styled-input" style="color: rgb(255, 0, 0); font-weight: bold; height: 30px; width: 120px;" :value="nakliye"  />
+                  </div>
+                </vs-td>
+              </div>
+              <div>
+                <vs-th style="color: black; font-weight: bold;">Ödeme Şekli</vs-th>
+                <vs-td>
+                  <div>
+                    <input type="text" class="styled-input" style="color: rgb(232, 0, 0); font-weight: bold; height: 30px; width: 120px;" :value="odemeSekli" disabled />
+                  </div>
+                </vs-td>
+              </div>
+            </div>
+            
             <div class="button-container" style="margin-left: 1292px; display: flex;">
               <vs-button style="border-radius: 5px; margin-right: 10px;" type="gradient" color="primary" @click="teklifYazdir()">Teklif Yazdır</vs-button>
               <vs-button style="border-radius: 5px;" type="gradient" color="success" @click="teklifKaydet()">Teklif Kaydet</vs-button>
@@ -174,6 +265,9 @@ export default {
   },
   data() {
     return {
+      gecerlilikSuresi: 1, // Default değeri burada 1 olarak ayarlanmıştır
+      nakliye: "Nakliye DAHİL",
+      odemeSekli: "PEŞİN",
       iskonto: 0,
       tutar: 0,
       kdvOrani: 20, // %20 KDV oranı
